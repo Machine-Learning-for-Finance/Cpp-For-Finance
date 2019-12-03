015cfb814397a1946df1e8e8b63b71f310cbc7f5
Gary Engler <gary.engler@capco.com>
2019-12-03 14:31:32 -0500

    Initial Inclusion of SimpleMakeFile

---
+++ 00-Makefiles/SimpleMakefile/include/module1/mod1c1.hpp
@@ -0,0 +1,14 @@
+#ifndef MOD1C1_HPP
+#define MOD1C1_HPP
+
+#include <iostream>
+
+class mod1c1
+{
+public:
+   void foo(); // { std::cout << "mod1c1\n"; }
+
+};
+
+#endif
+
---
+++ 00-Makefiles/SimpleMakefile/src/program.cpp
@@ -0,0 +1,25 @@
+#include <module1/mod1c1.hpp>
+#include <module1/mod1c2.hpp>
+#include <module2/mod2c1.hpp>
+#include <module2/mod2c2.hpp>
+#include <program.hpp>
+#include <thread>
+
+int main()
+{
+   mod1c1 m1c1;
+   mod1c2 m1c2;
+   mod2c1 m2c1;
+   mod2c2 m2c2;
+
+   std::thread t;
+
+   m1c1.foo();
+   m1c2.foo();
+   m2c1.foo();
+   m2c2.foo();
+   
+   program::foo();
+
+   return 0;
+}
